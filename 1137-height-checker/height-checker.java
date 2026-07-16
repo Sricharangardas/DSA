4acfea1748991d4a11229d01daa13920a657e73a
Sri Charan <sricharangardas@gmail.com>
2026-07-16 20:05:15 +0530

    Time: 4 ms (80.73%) | Memory: 43.7 MB (14.09%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] arr=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            arr[i]=heights[i];
+        }
+        Arrays.sort(arr);
+        int count=0;
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=heights[j]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
